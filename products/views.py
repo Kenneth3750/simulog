e33cf19dd95eb8c258d820266a352f2f11e12dfd
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-07-12 00:11:51 -0500

    error on previous commit

--- products/views.py
+++ products/views.py
@@ -47,7 +47,7 @@ def netWeight(request):
 def totalVolumePerProduct(request):
     serializer = TotalVolumeSerializer(data=request.data)
     if serializer.is_valid():
-        if serializer.validated_data["width"] > serializer.validated_data["length"]:
+        if serializer.validated_data["box_width"] > serializer.validated_data["box_length"]:
             return Response(
                 {'status': 'error', 
                 'message': 'Width must be less than length'},
